9d17a88be8d3d9c552020e2053e641b6c13b9ae5
Bar Lis <barlis@Bars-MacBook-Pro.local>
2024-01-15 18:21:14 +0200

    Try to fix 3d model on android

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -16,7 +16,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene} // Add the model to the scene
         scale={isMobile ? 9 : 15} // Increase the size of the model
-        position={isMobile ? [0, -1.5, 0] : [0, -2.8, 0]} // Move the model to the center of the scene
+        position={isMobile ? [0, -0.8, 0] : [0, -2.8, 0]} // Move the model to the center of the scene
         rotation={[0, 2.85, 0]} // Rotate the model
       />
     </mesh>
@@ -24,24 +24,37 @@ const Computers = ({ isMobile }) => {
 };
 
 const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false); // Check if the user is on mobile
+  // const [isMobile, setIsMobile] = useState(false); // Check if the user is on mobile
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 500);
+
+  const handleWindowSizeChange = () => {
+    setIsMobile(window.innerWidth <= 500);
+  };
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 500px)"); // Add a listener for changes in screen size
-
-    setIsMobile(mediaQuery.matches); // Set the state to true if the screen is smaller than 500px
-
-    const handleMediaQueryChange = (e) => {
-      // Function to handle changes in screen size
-      setIsMobile(e.matches); // Set the state to true if the screen is smaller than 500px
-    };
-
-    mediaQuery.addEventListener("change", handleMediaQueryChange); // Add event listener to the media query to check if the screen size changes
-
+    window.addEventListener("resize", handleWindowSizeChange);
     return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange); // Remove event listener when component unmounts
+      window.removeEventListener("resize", handleWindowSizeChange);
     };
   }, []);
+
+  // useEffect(() => {
+  //   const mediaQuery = window.matchMedia("(max-width: 500px)"); // Add a listener for changes in screen size
+
+  //   setIsMobile(mediaQuery.matches); // Set the state to true if the screen is smaller than 500px
+
+  //   const handleMediaQueryChange = (e) => {
+  //     // Function to handle changes in screen size
+  //     setIsMobile(e.matches); // Set the state to true if the screen is smaller than 500px
+  //   };
+
+  //   mediaQuery.addEventListener("change", handleMediaQueryChange); // Add event listener to the media query to check if the screen size changes
+
+  //   return () => {
+  //     mediaQuery.removeEventListener("change", handleMediaQueryChange); // Remove event listener when component unmounts
+  //   };
+  // }, []);
 
   return (
     <Canvas
@@ -60,6 +73,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2} // Don't go below the ground plane
           minPolarAngle={Math.PI / 2} // Don't go above the sky plane
         />
+
         <Computers isMobile={isMobile} />
       </Suspense>
 
